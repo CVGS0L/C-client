ace331383c995ac73023b0f7b55ecb8ed4848890
CVGS0L <tekkitboss112@gmail.com>
2019-04-09 10:11:58 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -15,11 +15,12 @@ class MainClass
     public static bool UserTrue = false;
     public static void Main()
     {
+        // this gets the HWID of the hard drive to use as a pseudo user name.
         ManagementObject dsk = new ManagementObject(@"win32_logicaldisk.deviceid=""c:""");
         dsk.Get();
         string id = dsk["VolumeSerialNumber"].ToString();
 
-
+        //set the console colors
         Console.BackgroundColor = ConsoleColor.White;
         Console.ForegroundColor = ConsoleColor.Green;
         Console.Clear();
@@ -27,27 +28,29 @@ class MainClass
 
 
        
-
-        string file = System.IO.File.ReadAllText(@"Data.pref");
-
+        // reads a file and saves it as a string
+        string file = System.IO.File.ReadAllText(@"<file (and drive) to open>");
+         // writes file to console
         System.Console.WriteLine(file);
-        // this works for now, keeping it
+        // gets user input
         string input = Console.ReadLine();
-
+        //checks for commands (does not work)
         if (input == "/admin clear") {
 
             string aClear = "";
             System.IO.File.WriteAllText(@"Data.pref", aClear);
 
         }
+        // sets HWID we got earlier as a username
         string username = id;
 
-
+        // adds new stuff to the old string.
         string newText = file + '\n' + username + ">> " + input;
-
-        System.IO.File.WriteAllText(@"Data.pref", newText);
+        // writes the new string to file that was opened earlier
+        System.IO.File.WriteAllText(@"<file (and drive) to open>", newText);
+        //closes the program and opens itself agian (to close the file so others can write to it, as well as acting as a refresh)
         Process neu = new Process();
-        neu.StartInfo.FileName = @"Chtrm.exe";
+        neu.StartInfo.FileName = @"<whatever you save this application as>";
         neu.Start();
         Environment.Exit(0);
 
